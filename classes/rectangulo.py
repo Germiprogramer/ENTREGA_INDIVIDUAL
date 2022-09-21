c639cbef2d74bfee36e36619a7912df01592616c
Germiprogramer <llorentemunozgerman@gmail.com>
2022-09-21 16:16:05 +0100

    creacion rectangulo en base a dos puntos

---
+++ classes/rectangulo.py
@@ -0,0 +1,19 @@
+from punto import *
+
+class Rectangulo():
+    def __init__(self, punto_1, punto_2):
+        
+        self.c_1 = (punto_1.x, punto_1.y)
+        self.c_2 = (punto_2.x, punto_1.y)
+        self.c_3 = (punto_1.x, punto_2.y)
+        self.c_4 = (punto_2.x, punto_2.y)
+
+    def base():
+        pass
+
+    def altura():
+        pass
+
+    def area():
+        pass
+
